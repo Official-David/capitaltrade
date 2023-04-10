21711d8b2537042fa6a2ce540c26d390b9befef1
official-david <adigwu12>
2023-04-10 23:07:54 +0100

    removed tawk.to api

--- resources/views/users/layouts/app.blade.php
+++ resources/views/users/layouts/app.blade.php
@@ -324,17 +324,7 @@
     <script src="../assets/js/component.js"></script>
 
     <!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/6428a00e31ebfa0fe7f5f78a/1gsvb2egk';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
+
 <!--End of Tawk.to Script-->
     @yield('pagejs')
 </body>
